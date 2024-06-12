0a4f9691a30810800de75e4664be6c01147bda40
Peter McEldowney <pcm1289@gmail.com>
2024-06-11 22:56:09 -0700

    add logging debug levels with -vv and version/hash using -V

--- Makefile
+++ Makefile
@@ -1,18 +1,29 @@
 CC = gcc
-CFLAGS += "-Wall"
+CFLAGS += -Wall -Werror -Wextra -g
+LDFLAGS ?=
+#LDFLAGS = -lssl -lcrypto
 OUTPUT ?= iceFUNprog
 PREFIX ?= /usr/local
+SRCS=*.c
 
+$(OUTPUT): $(SRCS)
+	$(CC) $(LDFLAGS) $(CFLAGS) -D_FILEHASH_MD5SUM=$(shell md5sum $(SRCS) | grep -o "^[0-9A-Fa-f]*[[:space:]]") -o $@ $^
+	@#$(CC) $(LDFLAGS) $(CFLAGS) -o $@ $^
 
-$(OUTPUT): *.c
-	$(CC) $(CFLAGS) -o $(OUTPUT) *.c
+# phony targets
+.PHONY: all
+all: $(OUTPUT)
+	@echo Target $(TARGET) build finished.
 
+.PHONY: install
 install: $(OUTPUT)
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	cp $(OUTPUT) $(DESTDIR)$(PREFIX)/bin/
 
+.PHONY: uninstall
 uninstall: $(OUTPUT)
 	rm $(DESTDIR)$(PREFIX)/bin/$(OUTPUT)
 
+.PHONY: clean
 clean:
 	rm -f *.o $(OUTPUT)
--- iceFUNprog.c
+++ iceFUNprog.c
@@ -15,6 +15,7 @@
  *
  */
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +31,19 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+#ifndef _FILEHASH_MD5SUM
+#include <openssl/md5.h>
+#else
+#define STRINGIFY(X) #X
+#define FILEHASH(X) STRINGIFY(X)
+#endif
+
+typedef enum {
+  ERROR,
+  WARNING,
+  INFO,
+  DEBUG,
+} debuglvl;
 
 enum cmds
 {
@@ -43,9 +57,29 @@ char ProgName[30];
 int fd;
 char verify;
 int rw_offset = 0;
+debuglvl g_debuglvl = WARNING;
+
+
+void dbgprintf(debuglvl level, const char* format, ...) {
+  if (level <= g_debuglvl) {
+    va_list args;
+    va_start(args, format);
+    switch (level) {
+      case ERROR:	fprintf(stderr, "  ERROR!| "); break;
+      case WARNING:	fprintf(stderr, " WARNING| "); break;
+      case INFO: 	fprintf(stderr, "    INFO| "); break;
+      case DEBUG: 	fprintf(stderr, "   DEBUG| "); break;
+	default:	fprintf(stderr, "        | "); break;
+    }
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fprintf(stderr, "\n");
+  }
+}
 
 static void help(const char *progname)
 {
+	dbgprintf(DEBUG, "showing help");
 	fprintf(stderr, "Programming tool for Devantech iceFUN board.\n");
 	fprintf(stderr, "Usage: %s [-P] <SerialPort>\n", progname);
 	fprintf(stderr, "       %s <input file>\n", progname);
@@ -58,7 +92,9 @@ static void help(const char *progname)
 	fprintf(stderr, "                         or 'M' for size in megabytes)\n");
 	fprintf(stderr, "                         or prefix '0x' to the argument for size in hexdecimal\n");
 	fprintf(stderr, "  --help\n");
-	fprintf(stderr, "  -v                    skip verification\n");
+	fprintf(stderr, "  -s                    skip verification\n");
+	fprintf(stderr, "  -V                    show version and binary build hash\n");
+	fprintf(stderr, "  -v                    increase verbosity\n");
 	fprintf(stderr, "Example:\n");
 	fprintf(stderr, "%s -P /dev/ttyACM1 blinky.bin\n", progname);
 	fprintf(stderr, "\n");
@@ -68,24 +104,53 @@ static void help(const char *progname)
 	fprintf(stderr, "\n");
 }
 
+#ifndef _FILEHASH_MD5SUM
+unsigned char* md5_sum(const char* filename) {
+  unsigned char* digest = (unsigned char*)malloc(MD5_DIGEST_LENGTH * sizeof(unsigned char));
+  FILE* file = fopen(filename, "rb");
+  if (!file) {
+    dbgprintf(ERROR, " Unable to open file %s", filename);
+    return NULL;
+  }
+
+  dbgprintf(DEBUG, "calculating MD5 hash of: %s", filename);
+  unsigned char buffer[1024];
+  MD5_CTX md5;
+  MD5_Init(&md5);
+  int bytes = 0;
+  int last_bytes = bytes;
+  while (((bytes += fread(buffer, 1, 1024, file)) - last_bytes) != 0) {
+    MD5_Update(&md5, buffer, bytes);
+    last_bytes = bytes;
+  }
+  MD5_Final(digest, &md5);
+  dbgprintf(DEBUG, "hash calculated from %d bytes", bytes);
+  dbgprintf(INFO, "hash calculated: %s", md5);
+  fclose(file);
+  return digest;
+}
+#endif
+
 int GetVersion(void)								// print firmware version number
 {
+	dbgprintf(DEBUG, "sending GET_VER message command: 0x%02x", GET_VER);
 	SerBuf[0] = GET_VER;
 	write(fd, SerBuf, 1);
 //	tcdrain(fd);
 	read(fd, SerBuf, 2);
 	if(SerBuf[0] == 38) {
-		fprintf(stderr, "iceFUN v%d, ",SerBuf[1]);
+		dbgprintf(INFO, "iceFUN v%d, ",SerBuf[1]);
 		return 0;
 	}
 	else {
-		fprintf(stderr, "%s: Error getting Version\n", ProgName);
+		dbgprintf(ERROR, "%s: Error getting Version", ProgName);
 		return EXIT_FAILURE;
 	}
 }
 
 int resetFPGA(void)									// reset the FPGA and return the flash ID bytes
 {
+	dbgprintf(DEBUG, "sending RESET_FPGA message command: 0x%02x", RESET_FPGA);
 	SerBuf[0] = RESET_FPGA;
 	write(fd, SerBuf, 1);
 	read(fd, SerBuf, 3);
@@ -95,6 +160,7 @@ int resetFPGA(void)									// reset the FPGA and return the flash ID bytes
 
 int releaseFPGA(void)								// run the FPGA
 {
+	dbgprintf(DEBUG, "sending RELEASE_FPGA message command: 0x%02x", RELEASE_FPGA);
 	SerBuf[0] = RELEASE_FPGA;
 	write(fd, SerBuf, 1);
 	read(fd, SerBuf, 1);
@@ -111,33 +177,47 @@ char portName[300];
 int length;
 struct termios config;
 
-	verify = 1;										// verify unless told not to
- 	DIR * d = opendir(portPath); 						// open the path
-	struct dirent *dir; 							// for the directory entries
-	if(d==NULL) strcpy(portName, "/dev/ttyACM0");		// default serial port to use
-	else {
-		while ((dir = readdir(d)) != NULL) 			// if we were able to read somehting from the directory
+	dbgprintf(DEBUG, "loading %s", __FILE__);
+	// verify unless told not to
+	verify = 1;
+
+	// open the path
+ 	DIR * d = opendir(portPath);
+
+	// for the directory entries
+	struct dirent *dir;
+	dbgprintf(DEBUG, "checking for port: %s", portName);
+	// default serial port to use
+	if(d==NULL) {
+	       	strcpy(portName, "/dev/ttyACM0");
+		dbgprintf(DEBUG, "using serial port: %s", portName);
+	} else {
+		// if we were able to read somehting from the directory
+		while ((dir = readdir(d)) != NULL)
 		{
 			char *pF = strstr(dir->d_name, "iceFUN");
 			if(pF) {
-				strcpy(portName, portPath);			// found iceFUN board so copy full symlink
+				// found iceFUN board so copy full symlink
+				strcpy(portName, portPath);
 				strcat(portName, dir->d_name);
+				dbgprintf(INFO, "found port: %s", portName);
 				break;
 			}
 		}
 	}
 	closedir(d);
 
-/* Decode command line parameters */
+	/* Decode command line parameters */
 	static struct option long_options[] = {
 		{"help", no_argument, NULL, -2},
 		{NULL, 0, NULL, 0}
 	};
 
 	int opt;
-  char *endptr;
-	while ((opt = getopt_long(argc, argv, "P:o:vh", long_options, NULL)) != -1) {
+	char *endptr;
+	while ((opt = getopt_long(argc, argv, "P:o:svVh", long_options, NULL)) != -1) {
 		switch (opt) {
+
 		case 'P': /* Serial port */
 			strcpy(portName, optarg);
 			break;
@@ -146,9 +226,36 @@ struct termios config;
 			help(argv[0]);
 			close(fd);
 			return EXIT_SUCCESS;
-		case 'v':
+		case 's':
 			verify = 0;
 			break;
+		case 'v':
+			g_debuglvl++;
+			dbgprintf(g_debuglvl == WARNING ? WARNING : g_debuglvl == INFO ? INFO : g_debuglvl == DEBUG ? DEBUG : ERROR, "increased debug level");
+			break;
+		case 'V':
+			fprintf(stdout, "Build datetime: %s   T %s\n", __DATE__, __TIME__);
+#ifndef _FILEHASH_MD5SUM
+			int strsize = strlen("Build hash (): ") + strlen(__FILE__) + 1;
+			char* progfile = malloc(strsize*sizeof(char));
+		        sprintf(progfile, "%s", __FILE__);
+			fprintf(stdout, "Build hash (%s): ", progfile);
+			struct stat buffer;
+  			if (stat(progfile, &buffer) == 0 && S_ISREG(buffer.st_mode)) {
+    				unsigned char* digest = md5_sum(progfile);
+	    			for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+	      				fprintf(stdout, "%02x", digest[i]);
+	    			}
+				free(digest);
+			} else {
+				fprintf(stdout, "[[ %s </not_found> ]]", __FILE__);
+			}
+			fprintf(stdout, "\n\n");
+			free(progfile);
+#else
+			fprintf(stdout, "Build hash: %s\n\n", FILEHASH(_FILEHASH_MD5SUM));
+#endif
+			return 0;
     case 'o': /* set address offset */
       rw_offset = strtol(optarg, &endptr, 0);
       if (*endptr == '\0')
@@ -158,13 +265,13 @@ struct termios config;
       else if (!strcmp(endptr, "M"))
         rw_offset *= 1024 * 1024;
       else {
-        fprintf(stderr, "'%s' is not a valid offset\n", optarg);
+        dbgprintf(ERROR, "'%s' is not a valid offset", optarg);
         return EXIT_FAILURE;
       }
       break;
 		default:
 			/* error message has already been printed */
-			fprintf(stderr, "Try `%s -h' for more information.\n", argv[0]);
+			dbgprintf(ERROR, "Try `%s -h' for more information.", argv[0]);
 			close(fd);
 			return EXIT_FAILURE;
 		}
@@ -172,7 +279,7 @@ struct termios config;
 
 	fd = open(portName, O_RDWR | O_NOCTTY);
 	if(fd == -1) {
-		fprintf(stderr, "%s: failed to open serial port.\n", argv[0]);
+		dbgprintf(ERROR, "%s: failed to open serial port.", argv[0]);
 		return EXIT_FAILURE;
 	}
 	tcgetattr(fd, &config);
@@ -182,20 +289,20 @@ struct termios config;
 	if (optind + 1 == argc) {
 		filename = argv[optind];
 	} else if (optind != argc) {
-		fprintf(stderr, "%s: too many arguments\n", argv[0]);
-		fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+		dbgprintf(ERROR, "%s: too many arguments", argv[0]);
+		dbgprintf(ERROR, "Try `%s --help' for more information.\n", argv[0]);
 		close(fd);
 		return EXIT_FAILURE;
 	} else  {
-		fprintf(stderr, "%s: missing argument\n", argv[0]);
-		fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+		dbgprintf(ERROR, "%s: missing argument", argv[0]);
+		dbgprintf(ERROR, "Try `%s --help' for more information.\n", argv[0]);
 		close(fd);
 		return EXIT_FAILURE;
 	}
 
 	FILE* fp = fopen(filename, "rb");
 	if(fp==NULL) {
-		fprintf(stderr, "%s: failed to open file %s.\n", argv[0], filename);
+		dbgprintf(ERROR, "%s: failed to open file %s.", argv[0], filename);
 		close(fd);
 		return EXIT_FAILURE;
 	}
@@ -206,20 +313,23 @@ struct termios config;
 	else return EXIT_FAILURE;
 
   int endPage = ((rw_offset + length) >> 16) + 1;
+
+  dbgprintf(DEBUG, "starting page erase operation, ERASE_64k command: 0x%02x", ERASE_64k);
   for (int page = (rw_offset >> 16); page < endPage; page++)			// erase sufficient 64k sectors
   {
     SerBuf[0] = ERASE_64k;
     SerBuf[1] = page;
     write(fd, SerBuf, 2);
-    fprintf(stderr,"Erasing sector %02X0000\n", page);
+    dbgprintf(INFO, "Erasing sector %02X0000", page);
     read(fd, SerBuf, 1);
   }
-  fprintf(stderr, "file size: %d\n", (int)length);
+  dbgprintf(INFO, "file size: %d", (int)length);
 
 	int addr = rw_offset;
-	fprintf(stderr,"Programming ");						// program the FPGA
+	dbgprintf(INFO ,"Programming ");						// program the FPGA
 	int cnt = 0;
-  int endAddr = addr + length; // no flashsize check
+  	int endAddr = addr + length; // no flashsize check
+	dbgprintf(DEBUG, "starting programming. PROG_PAGE command is: 0x%02x", PROG_PAGE);
 	while (addr < endAddr)
 	{
 		SerBuf[0] = PROG_PAGE;
@@ -231,20 +341,23 @@ struct termios config;
 		read(fd, SerBuf, 4);
 		if (SerBuf[0] != 0)
 		{
-			fprintf(stderr,"\nProgram failed at %06X, %02X expected, %02X read.\n", addr - 256 + SerBuf[1] - 4, SerBuf[2], SerBuf[3]);
+			dbgprintf(ERROR, "\nProgram failed at %06X, %02X expected, %02X read.", addr - 256 + SerBuf[1] - 4, SerBuf[2], SerBuf[3]);
 			return EXIT_FAILURE;
 		}
 		if (++cnt == 10)
 		{
 			cnt = 0;
-			fprintf(stderr,".");
+			fprintf(stderr, ".");
 		}
 	}
+	fprintf(stderr, "\n");
 
 	if(verify) {
 		addr = rw_offset;
-		fprintf(stderr,"\nVerifying ");
+		dbgprintf(INFO, "Verifying ");
 		cnt = 0;
+
+		dbgprintf(DEBUG, "starting page verification, VERIFY_PAGE command: 0x%02x", VERIFY_PAGE);
 		while (addr < endAddr)
 		{
 			SerBuf[0] = VERIFY_PAGE;
@@ -256,17 +369,17 @@ struct termios config;
 			read(fd, SerBuf, 4);
 			if (SerBuf[0] > 0)
 			{
-				fprintf(stderr,"\nVerify failed at %06X, %02X expected, %02X read.\n", addr - 256 + SerBuf[1] - 4, SerBuf[2], SerBuf[3]);
+				dbgprintf(ERROR, "Verify failed at %06X, %02X expected, %02X read.", addr - 256 + SerBuf[1] - 4, SerBuf[2], SerBuf[3]);
 				return EXIT_FAILURE;
 			}
 			if (++cnt == 10)
 			{
 				cnt = 0;
-				fprintf(stderr,".");
+				fprintf(stderr, ".");
 			}
 		}
 	}
-	fprintf(stderr,"\n");
+	fprintf(stderr, "\n");
 
 	releaseFPGA();
 	return 0;
